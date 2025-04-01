486c1c76957e2f27947ea21072255b81e81485d3
kARTHIKpUJAARI <karthikpujari41@gmail.com>
2025-04-01 10:22:01 +0530

    Event handling by onClick button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-// import ConditionalComponent from "./components/ConditionalComponent";
 import ConditionalComponent from "./components/ConditionalComponent";
 import Fruits from "./components/Fruits";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return(
     <div>
-      <Fruits/>
-      <ConditionalComponent />
+      <Message />
     </div>
   );
   
